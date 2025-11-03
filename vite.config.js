b9666fa2b6c14b2dd1d8bab1b7ae6ed6e5705a89
吴鹏煜 <3265086498@qq.com>
2025-11-03 12:50:43 +0800

    Add Vue3 frontend

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: './',
+  build: {
+    outDir: 'dist', // 输出目录（默认就是 dist）
+  },
   server: {
     port: 5173,
   }
